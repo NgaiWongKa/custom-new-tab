823a1d42b81813dcac11ee1eab564c0b6a7757d1
Wong Ka Ngai <ngawy010@mymail.unisa.edu.au>
2025-07-07 18:29:36 +0930

    3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("search").addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
     const query = this.value.trim();
     if (query) {
-      window.location.href = `https://duckduckgo.com/?q=${encodeURIComponent(query)}`;
+      window.location.href = `https://www.startpage.com/do/dsearch?q=${encodeURIComponent(query)}&cat=web&language=english`;
     }
   }
 });
